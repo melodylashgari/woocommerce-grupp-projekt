90faf83312908ad053daf612f366810b53bff83b
Rebecka <90262966+RebeckaGamble@users.noreply.github.com>
2022-10-04 13:57:29 +0200

    tagit bort från page

--- wp-content/themes/webshop/page.php
+++ wp-content/themes/webshop/page.php
@@ -1,38 +1,6 @@
 <?php get_header(); ?>
 
-<!-- Om oss -->
-<?php if(is_page(72)): ;?>
-	<?php
-		$page_title = get_field('page_title');
-		$image = get_field('image');
-		$size = 'large';
-		$om_oss_title = get_field('om_oss_title');
-		$om_oss_text = ('om_oss_text');
-	?>
-	<div class="om-oss">
-		<h1> <?php the_field('page_title'); ?></h1>
-		
-		<div class="about-img">
-			<?php if (!empty($image)): ?>
-				<img src="<?php echo esc_url($image['url']); ?>" />
-			<?php endif; ?>
-		</div>
-		<div class="about-content">
-			<strong> <?php the_field('om_oss_title') ?> </strong>
-			<br>
-			<?php the_field('om_oss_text') ?>
-		</div>
-	</div>
-<?php endif; ?>
 
-<!-- kontakta oss -->
-<?php if(is_page(118)): ;?>
-	<div class="kontakta-oss">
-		
-		<?php the_content( ); ?>
-		
-	</div>
-<?php endif; ?>
 
 <ul class="products">
 	<?php
